82ca0ab79bba8d7a952b534f1d585b4cda43af62
Peter Trobec <peter.trobec@studio-moderna.com>
2017-03-01 20:27:05 +0100

    Dirty commit of debounce on buttons

--- chickenCoopDoor/chickenCoopDoor.ino
+++ chickenCoopDoor/chickenCoopDoor.ino
@@ -1,26 +1,38 @@
+#include <Bounce2.h>
+
 const int goUpButtonInput = 2;
 const int goDownButtonInput = 3;
 const int goLeftOutput = 7;
 const int goRightOutput = 8;
 const int enableMotorA = 9;
-int buttonUpState = 0;
-int buttonDownState = 0;
+
+Bounce debouncerButtonUp = Bounce();
+Bounce debouncerButtonDown = Bounce();
 
 void setup() {
   pinMode(goUpButtonInput, INPUT);
   pinMode(goDownButtonInput, INPUT);
   pinMode(goLeftOutput, OUTPUT);
   pinMode(goRightOutput, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
 
   digitalWrite(goUpButtonInput, HIGH);
   digitalWrite(goDownButtonInput, HIGH);
+
+  digitalWrite(goLeftOutput, LOW);
+  digitalWrite(goRightOutput, LOW);
+
+  debouncerButtonUp.attach(goUpButtonInput);
+  debouncerButtonDown.attach(goDownButtonInput);
+  debouncerButtonUp.interval(5);
+  debouncerButtonDown.interval(5);
 }
 
 void loop() {
   digitalWrite(enableMotorA, HIGH);
-  buttonUpState = digitalRead(goUpButtonInput);
-
+  
+  debouncerButtonUp.update();
+  debouncerButtonDown.update();
+  
   if (isDark())
   {
     if (!isDoorFullyClosed())
@@ -37,6 +49,8 @@ void loop() {
       motorStop();
     }
   }
+  
+  delay(10);
 }
 
 bool isDark()
@@ -52,36 +66,42 @@ bool isDark()
 
 bool isDoorFullyClosed()
 {
-  buttonDownState = digitalRead(goDownButtonInput);
-  if (buttonDownState == LOW) {
+  int value = debouncerButtonDown.read();
+  
+  if (value == 0) {
     return true;
   }
+  
   return false;
 }
 
 bool isDoorFullyOpen()
 {
-  buttonUpState = digitalRead(goUpButtonInput);
-  if (buttonUpState == LOW) {
+  int value = debouncerButtonUp.read();
+  
+  if (value == 0) {
     return true;
   }
+  
   return false;
 }
 
 void motorStop()
 {
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
+  digitalWrite(enableMotorA, LOW);
+  digitalWrite(goLeftOutput, LOW);
+  digitalWrite(goRightOutput, LOW);
 }
 
 void moveDoorUp()
 {
-  digitalWrite(7, HIGH);
-  digitalWrite(8, LOW);
+  digitalWrite(goLeftOutput, HIGH);
+  digitalWrite(goRightOutput, LOW);
 }
 
 void moveDoorDown()
 {
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
+  digitalWrite(goLeftOutput, LOW);
+  digitalWrite(goRightOutput, HIGH);
 }
+
